dc9b257772fb71b50d9ed2c9cdcd66e5a3a17199
Maciej Matula <maciek.matula@gmail.com>
2022-05-26 10:02:58 -0500

    commit tree d3f9bd0b5047e642135c0d0f786ba26ae03d7eda

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event){
+        const newArray = [];
+    for(let i = 0; i < array.length; i++){
+        newArray[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`
+    }
+    return newArray
+}
+
+function countDown(integer){
+    let i = integer;
+    while (i >= 0){
+        console.log(i);
+        i--
+    }
+}
